4ca688648a8675d6eb3382446e0f31948a2f54b1
surajkus <kushwahasuraj752@gmail.com>
2025-09-03 12:16:41 +0530

    day1_py

---
+++ index.py
@@ -0,0 +1,41 @@
+
+# practice set of python
+# 1
+a = 10
+b = 30
+if a>b:
+    print("A is greater than B")
+
+else:
+    print("B is greater than A")    
+
+
+# # 2
+c = str(input("Enter your Gender: "))
+
+if c=="male":
+    print("Hello sir good morning: ")
+elif c=="Female":
+    print("Hello maam good morning: ")
+
+else:
+    print("Hello sir: ")    
+
+# 3 check odd Or not even
+
+number=int(input("Enter a number:= "))
+if number//2:
+    print("this is even number")
+
+else:
+    print("This is a odd number")    
+
+
+
+ # (4) check the student are eligible for vote  :
+
+num =int(input("Enter your age = "))
+if num>18:
+    print("You are eligible for vote")
+else:
+    print("You are not eligible for vote")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+a = 10
+b = 30
+if a>b:
+    print("A is greater than B")
+
+else:
+    print("B is greater than A")    
+
+
+# # 2
+c = str(input("Enter your Gender: "))
+
+if c=="male":
+    print("Hello sir good morning: ")
+elif c=="Female":
+    print("Hello maam good morning: ")
+
+else:
+    print("Hello sir: ")  
