9f871f3a4614b25f99c2e2e3fb6346a167c6af94
surajkus <kushwahasuraj752@gmail.com>
2025-09-06 09:09:50 +0530

    day_3.1

--- loopsCondtion.py
+++ loopsCondtion.py
@@ -2,3 +2,16 @@
 n=int(input("Enter  number = "))
 for i in range(n,n*11,n):
     print(i)
+
+# find length of strings
+
+#  (a) print the string using the indexing value
+name ="SUraj"
+for i in range(len(name)):
+    print(name[i])
+
+# (b) print the value using the direct over the string
+
+a = "kushwaha"
+for i in a:
+    print(i)
